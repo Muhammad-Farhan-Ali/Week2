f939e32f8490667b908c7881a83d6d3073b0aa8a
Muhammad-Farhan-Ali <mfarhanali672@gmail.com>
2024-09-12 08:47:15 -0700

    message

---
+++ awards.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+main()
+{
+
+	system("color 57");
+	cout<<" ...                    eeeee    eeeee            ...........    "<<endl;
+	cout<<" ....                  e     e  e     e           ..........     "<<endl;
+	cout<<" .....                    eee   e     e           .........      "<<endl;
+	cout<<" ......                  ee     e     e           ........       "<<endl;
+	cout<<" .......               eeeeeeee  eeeee   th       .......        "<<endl;
+	cout<<"  .......         ...........................     ......         "<<endl;
+	cout<<"    ......         C   E   N   T   U   R   Y      .....          "<<endl;
+	cout<<"      .....       ...........................     ....           "<<endl;
+	cout<<"         ...        eeeee eeeee e   e eeeee       ...            "<<endl;
+	cout<<"           ..         e   e      e e    e         ..             "<<endl;
+	cout<<"         __||__       e   eeeee   e     e       __||__           "<<endl;
+	cout<<"        |      |      e   e      e e    e      |      |          "<<endl;
+	cout<<"________|______|__    e   eeeee e   e   e    __|______|________  "<<endl;
+
+}
---
+++ name.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	cout<<"My name is Farhan\nI am from Lahore";
+	getch();
+	return 0;
+}
